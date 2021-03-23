0a3981d15beac928f47ab916c2a3cc59a6c056da
KaterinRocks <katerock25@gmail.com>
2021-03-23 08:09:50 +0800

    DZ 1.1. fixed

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -22,8 +22,8 @@ function averageMark(a, g, p) {
 function sayHello(userName) {
 	
     let myName = userName;
-	let message = "Привет, мир! Меня зовут," + " " + myName + ".";
-
+	//let message = "Привет, мир! Меня зовут" + " " + myName + ".";
+	let message = `Привет, мир! Меня зовут ${myName}.`
 	return message;
 }
 
